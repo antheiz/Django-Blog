e0ccc1eda154651e507d10184cfb835ae62121b5
antheiz <salupacode@gmail.com>
2023-04-26 14:09:39 +0700

    feat: add user profile images

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from users.models import CustomUser
+from users.models import CustomUser, Profile
 
-admin.site.register(CustomUser)
+admin.site.register([CustomUser, Profile])
--- users/models.py
+++ users/models.py
@@ -6,3 +6,11 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    profile_img = models.ImageField(default="default.jpg", upload_to="profile_pics")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
